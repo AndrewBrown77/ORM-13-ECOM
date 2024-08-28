9cd1823a3d7ac4722a192f9902c2b4e5a628bb09
andrew-brown <andrewmbrown97@gmail.com>
2024-08-27 21:02:10 -0400

    commit

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement:true,
+      allowNull: false, 
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{ isDecimal: true },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{ isNumeric: true},
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        // This references the `category` model, which we set in `Category.js` as its `category_id` property
+        model: 'category',
+        key: 'id',
+      },
+    },
+
   },
   {
     sequelize,
